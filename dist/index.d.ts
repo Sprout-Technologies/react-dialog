b755c903a0d958ba08a8c55af247bd427b41bb3d
Steven Kao <st.kao.05@gmail.com>
2022-06-06 21:26:45 +0800

    Fix type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,63 +6,91 @@ import * as Polymorphic from "@radix-ui/react-polymorphic";
 type DismissableLayerProps = React.ComponentProps<typeof DismissableLayer>;
 type FocusScopeProps = React.ComponentProps<typeof FocusScope>;
 type DialogOwnProps = {
-    open?: boolean;
-    defaultOpen?: boolean;
-    onOpenChange?(open: boolean): void;
+  open?: boolean;
+  defaultOpen?: boolean;
+  onOpenChange?(open: boolean): void;
+  children: React.ReactNode;
 };
 export const Dialog: React.FC<DialogOwnProps>;
 declare const TRIGGER_DEFAULT_TAG = "button";
 type DialogTriggerOwnProps = Polymorphic.OwnProps<typeof Primitive>;
-type DialogTriggerPrimitive = Polymorphic.ForwardRefComponent<typeof TRIGGER_DEFAULT_TAG, DialogTriggerOwnProps>;
+type DialogTriggerPrimitive = Polymorphic.ForwardRefComponent<
+  typeof TRIGGER_DEFAULT_TAG,
+  DialogTriggerOwnProps
+>;
 export const DialogTrigger: DialogTriggerPrimitive;
-type DialogOverlayOwnProps = Polymorphic.Merge<Polymorphic.OwnProps<typeof DialogOverlayImpl>, {
+type DialogOverlayOwnProps = Polymorphic.Merge<
+  Polymorphic.OwnProps<typeof DialogOverlayImpl>,
+  {
     /**
      * Used to force mounting when more control is needed. Useful when
      * controlling animation with React animation libraries.
      */
     forceMount?: true;
-}>;
-type DialogOverlayPrimitive = Polymorphic.ForwardRefComponent<Polymorphic.IntrinsicElement<typeof DialogOverlayImpl>, DialogOverlayOwnProps>;
+  }
+>;
+type DialogOverlayPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof DialogOverlayImpl>,
+  DialogOverlayOwnProps
+>;
 export const DialogOverlay: DialogOverlayPrimitive;
 type DialogOverlayImplOwnProps = Polymorphic.OwnProps<typeof Primitive>;
-type DialogOverlayImplPrimitive = Polymorphic.ForwardRefComponent<Polymorphic.IntrinsicElement<typeof Primitive>, DialogOverlayImplOwnProps>;
+type DialogOverlayImplPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Primitive>,
+  DialogOverlayImplOwnProps
+>;
 declare const DialogOverlayImpl: DialogOverlayImplPrimitive;
-type DialogContentOwnProps = Polymorphic.Merge<Polymorphic.OwnProps<typeof DialogContentImpl>, {
+type DialogContentOwnProps = Polymorphic.Merge<
+  Polymorphic.OwnProps<typeof DialogContentImpl>,
+  {
     /**
      * Used to force mounting when more control is needed. Useful when
      * controlling animation with React animation libraries.
      */
     forceMount?: true;
-}>;
-type DialogContentPrimitive = Polymorphic.ForwardRefComponent<Polymorphic.IntrinsicElement<typeof DialogContentImpl>, DialogContentOwnProps>;
+  }
+>;
+type DialogContentPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof DialogContentImpl>,
+  DialogContentOwnProps
+>;
 export const DialogContent: DialogContentPrimitive;
-type DialogContentImplOwnProps = Polymorphic.Merge<Polymorphic.OwnProps<typeof Primitive>, {
+type DialogContentImplOwnProps = Polymorphic.Merge<
+  Polymorphic.OwnProps<typeof Primitive>,
+  {
     /**
      * Event handler called when auto-focusing on open.
      * Can be prevented.
      */
-    onOpenAutoFocus?: FocusScopeProps['onMountAutoFocus'];
+    onOpenAutoFocus?: FocusScopeProps["onMountAutoFocus"];
     /**
      * Event handler called when auto-focusing on close.
      * Can be prevented.
      */
-    onCloseAutoFocus?: FocusScopeProps['onUnmountAutoFocus'];
+    onCloseAutoFocus?: FocusScopeProps["onUnmountAutoFocus"];
     /**
      * Event handler called when the escape key is down.
      * Can be prevented.
      */
-    onEscapeKeyDown?: DismissableLayerProps['onEscapeKeyDown'];
+    onEscapeKeyDown?: DismissableLayerProps["onEscapeKeyDown"];
     /**
      * Event handler called when the a pointer event happens outside of the `Dialog`.
      * Can be prevented.
      */
-    onPointerDownOutside?: DismissableLayerProps['onPointerDownOutside'];
-}>;
-type DialogContentImplPrimitive = Polymorphic.ForwardRefComponent<Polymorphic.IntrinsicElement<typeof Primitive>, DialogContentImplOwnProps>;
+    onPointerDownOutside?: DismissableLayerProps["onPointerDownOutside"];
+  }
+>;
+type DialogContentImplPrimitive = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Primitive>,
+  DialogContentImplOwnProps
+>;
 declare const DialogContentImpl: DialogContentImplPrimitive;
 declare const CLOSE_DEFAULT_TAG = "button";
 type DialogCloseOwnProps = Polymorphic.OwnProps<typeof Primitive>;
-type DialogClosePrimitive = Polymorphic.ForwardRefComponent<typeof CLOSE_DEFAULT_TAG, DialogCloseOwnProps>;
+type DialogClosePrimitive = Polymorphic.ForwardRefComponent<
+  typeof CLOSE_DEFAULT_TAG,
+  DialogCloseOwnProps
+>;
 export const DialogClose: DialogClosePrimitive;
 export const Root: React.FC<DialogOwnProps>;
 export const Trigger: DialogTriggerPrimitive;
